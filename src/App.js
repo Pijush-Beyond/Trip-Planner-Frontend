e5ba91376bb1b21b0fe2812c22c16732a7e67de8
SoumyaD98 <soumyamsd7@gmail.com>
2021-05-01 00:09:41 +0530

    login and register commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+
+//import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link, Redirect} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Login from './components/login'
+import Register from './components/register'
+//import Navi from './Components/Navi'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+    
+    <Switch>
+      <Route exact path="/" component={Login}/>
+      <Route exact path="/register" component={Register}/>
+    </Switch>
+    </Router>
   );
 }
 
